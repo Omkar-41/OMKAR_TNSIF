a39c09f721fca1c4d529eeb5f05a980a3e6cbff4
Omkar Chemate <omkarbchemate@gmail.com>
2024-10-03 16:27:42 +0530

    Day 1 demo

---
+++ Day_01_03_Oct/src/com/tnsif/dayone/firstprogram/FirstProgram.java
@@ -0,0 +1,13 @@
+package com.tnsif.dayone.firstprogram;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello./...");
+		System.out.println("-----Welcome to TNSF");
+		
+	}
+
+}
